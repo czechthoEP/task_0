f122427def5efa536085e4f55ceaa580beb42235
Thomas <czechtho@hs-pforzheim.de>
2024-11-24 23:34:54 +0100

    added initial junit test file

---
+++ src/test/java/org/task/MainTest.java
@@ -0,0 +1,18 @@
+package org.task;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getClosestValueToZero() {
+    }
+}
